cc81115c86b0a94def6d1e216fce61c53b99a60b
David Dang <dtudang@gmail.com>
2015-02-17 15:58:02 -0800

    added notification messages for creation and deletion of notes

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  message: "Welcome to the notes app",
   actions: {
     createNote: function() {
       var text= this.get('newText');
       var note= this.store.createRecord('note', {text:text});
       note.save();
       this.set('newText', '');
+      this.set('message', "Successfully create a note!")
     },
     deleteNote: function(note){
       note.destroyRecord();
+      this.set('message', "Deleted that note!")
     }
   }
 });

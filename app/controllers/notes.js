f4857503f29d784847e1475dc09f19ebb8d5201e
David Dang <dtudang@gmail.com>
2015-02-18 10:22:05 -0800

    added titles to notes

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -2,12 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   message: "Welcome to the notes app",
+  isValid: Ember.computed.notEmpty('newText'),
   actions: {
     createNote: function() {
+      var title= this.get('newTitle');
       var text= this.get('newText');
-      var note= this.store.createRecord('note', {text:text});
+      var note= this.store.createRecord('note', {text:text, title:title});
       note.save();
       this.set('newText', '');
+      this.set('newTitle', '')
       this.set('message', "Successfully create a note!")
     },
     deleteNote: function(note){
